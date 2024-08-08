fe0ffba868e53ef51463cd15062de38a71ed3f6f
Arunkumar Dhanasekar <darunkumar@vg.com>
2024-08-08 18:19:06 +0530

    Done with fun

---
+++ Git_Details/src/firstPackage/sample.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class sample {
+	
+	static int a=100;
+	
+	public static void main (String[] args) {
+		System.out.println("The value of a is" +" "+ a);
+	}
+
+}
---
+++ Git_Details/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author darunkumar
+ *
+ */
+module Git_Details {
+}
